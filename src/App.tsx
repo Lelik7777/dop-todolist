a409535dffbb3bb02f0f9646e44d450a8acc4247
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-12-02 09:01:21 +0300

    add some corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,42 @@
-import React, {useReducer, useState} from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {addTaskAC, changeStatusAC, removeTaskAC, taskReducer} from './components/redux/taskReducer';
 import {changeFilterAC, filterReducer} from './components/redux/filterReducer';
 
-export type FilterValuesType = "all" | "active" | "completed";
+export type FilterValuesType = 'all' | 'active' | 'completed';
 
 function App() {
     let [tasks, tasksDispatch] = useReducer(taskReducer,
-        [{id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},]
+        [{id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
+            {id: v1(), title: 'Rest API', isDone: false},
+            {id: v1(), title: 'GraphQL', isDone: false},]
     );
 
-    let [filter, filterDispatch] = useReducer(filterReducer,"all");
+    let [filter, filterDispatch] = useReducer(filterReducer, 'all');
 
     function removeTask(id: string) {
-        //let filteredTasks = tasks.filter(t => t.id != id);
         tasksDispatch(removeTaskAC(id));
     }
 
     function addTask(title: string) {
-       // let task = {id: v1(), title: title, isDone: false};
-       // let newTasks = [task, ...tasks];
         tasksDispatch(addTaskAC(title));
     }
 
     const changeStatus = (CurrentId: string, value: boolean) => {
-        console.log(value)
-        tasksDispatch(changeStatusAC(CurrentId,value))
-       // tasksDispatch(tasks.map(m => m.id === CurrentId ? {...m, isDone: value} : m))
+        tasksDispatch(changeStatusAC(CurrentId, value))
     }
-
 
 
     let tasksForTodolist = tasks;
 
-    if (filter === "active") {
+    if (filter === 'active') {
         tasksForTodolist = tasks.filter(t => t.isDone === false);
     }
-    if (filter === "completed") {
+    if (filter === 'completed') {
         tasksForTodolist = tasks.filter(t => t.isDone === true);
     }
 
--- src/components/redux/taskReducer.ts
+++ src/components/redux/taskReducer.ts
@@ -4,26 +4,26 @@ import {v1} from 'uuid';
 export const taskReducer = (state: TaskType[], action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TASKS':
-            return [...state.filter(t => t.id != action.id)];
+            return state.filter(t => t.id !== action.payload.id);
         case 'ADD-TASK':
             let task = {id: v1(), title: action.title, isDone: false};
-            return [...state,task];
-        case 'CHAGE-STATUS':
-            return [...state.map(m => m.id === action.payload.id ? {...m, isDone: action.payload.value} : m)];
+            return [...state, task];
+        case 'CHANGE-STATUS':
+            return state.map(m => m.id === action.payload.id ? {...m, isDone: action.payload.value} : m);
         default:
-            return state;
+            return [...state];
     }
 }
 //automatic create type
-type ActionType =ReturnType<typeof removeTaskAC|typeof addTaskAC|typeof changeStatusAC>
+type ActionType = ReturnType<typeof removeTaskAC | typeof addTaskAC | typeof changeStatusAC>
 
 export const removeTaskAC = (id: string) => {
     return {
         type: 'REMOVE-TASKS',
-        id,
-    }as const
+        payload: {id},
+    } as const
 };
 
-export const addTaskAC = (title:string) => ({type:'ADD-TASK',title})as const
+export const addTaskAC = (title: string) => ({type: 'ADD-TASK', title}) as const
 
-export const changeStatusAC = (id:string,value:boolean) => ({type:'CHAGE-STATUS', payload:{id,value}})as const
+export const changeStatusAC = (id: string, value: boolean) => ({type: 'CHANGE-STATUS', payload: {id, value}}) as const
